e463d4471712e633e57f137ce524e03cbfb6a0a4
Joshua Ma <majosh@umich.edu>
2025-06-16 11:02:50 -0400

    Revert back to duplicates

--- gacha/database.py
+++ gacha/database.py
@@ -103,7 +103,6 @@ async def exec_write(query, params=None, commit=True):
     async with pool.connection() as conn:
         async with conn.cursor(row_factory=dict_row) as cur:
             try:
-                await cur.execute("SET TIME ZONE 'US/Eastern';")
                 await cur.execute(query, params)
 
                 if commit:
@@ -176,7 +175,7 @@ async def set_last_status():
 
 async def get_num_gifs(user):
     query = """
-            SELECT SUM(amount) FROM user_gifs
+            SELECT COUNT(*) FROM user_gifs
             WHERE user_id = %s;
             """
 
@@ -187,7 +186,7 @@ async def get_num_gifs(user):
 
 async def get_num_tier_gifs(user, tier):
     query = """
-            SELECT SUM(amount) 
+            SELECT COUNT(*)
             FROM user_gifs
             INNER JOIN gifs ON user_gifs.gif_id = gifs.id
             WHERE user_gifs.user_id = %s AND gifs.tier = %s;
@@ -424,10 +423,6 @@ async def add_user_gif(user_info, gif):
     query = """
             INSERT INTO user_gifs (user_id, gif_id)
             VALUES (%s, %s)
-            ON CONFLICT (user_id, gif_id)
-            DO UPDATE SET
-            amount = user_gifs.amount + 1,
-            new_obtain_date = NOW();
             """
 
     await exec_write(query, params=(user_info['user_id'], gif['id'],))

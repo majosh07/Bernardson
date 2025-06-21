d9b8d739cf65675ae734c918022fcb3cb6d7f358
Joshua Ma <majosh@umich.edu>
2025-06-21 16:29:44 -0400

    working get user

--- website/views/inventory.py
+++ website/views/inventory.py
@@ -7,12 +7,12 @@ from logging_config import logger
 inventory_bp = Blueprint('inventory', __name__, url_prefix='/user')
 
 @inventory_bp.route('/<name>')
-async def index(name):
+def index(name):
     query = """
             SELECT * FROM users
             WHERE username = %s;
             """
-    user = await fetch_dict(query, params=(name,))
+    user = fetch_dict(query, params=(name,))
     logger.info(user)
     if user is None:
         flask.abort(404)
